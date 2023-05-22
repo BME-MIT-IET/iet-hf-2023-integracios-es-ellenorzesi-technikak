8c791deaffb076c250892e33159c5000ce6a970c
jakymatyas <jaky.matyas@gmail.com>
2023-05-22 20:52:00 +0200

    asd

---
+++ unitTests/game/GameTests.java
@@ -0,0 +1,19 @@
+package game;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameTests {
+
+    @Before
+    public void inti(){
+
+    }
+
+    @Test
+    public void  GameTest1(){
+
+    }
+}

d854382061576139642110b921ef548ed2065463
Sámuel Csányi <samuel.csanyi@gmail.com>
2023-05-31 23:04:08 +0200

    tesztek sikeresen átültetve v2

--- src/test/java/hu/bme/iet_hf_group/wizard/uiTests/MenuFrameTest.java
+++ src/test/java/hu/bme/iet_hf_group/wizard/uiTests/MenuFrameTest.java
@@ -1,106 +1,113 @@
-package hu.bme.iet_hf_group.wizard.uiTests;
 
-import hu.bme.iet_hf_group.wizard.graphics.GameFrame;
-import hu.bme.iet_hf_group.wizard.graphics.MenuFrame;
-import org.assertj.swing.core.GenericTypeMatcher;
-import org.assertj.swing.edt.GuiActionRunner;
-import org.assertj.swing.fixture.FrameFixture;
-import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
-import org.junit.Before;
-import org.junit.Test;
+import hu.bme.iet_hf_group.wizard.graphics.StartButtonActionListener;
 
+import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
-import static org.assertj.swing.finder.WindowFinder.findFrame;
+/**
+ * A főmenüt megjelenítő ablak osztály
+ */
+public class MenuFrame extends JFrame {
 
-public class MenuFrameTest extends AssertJSwingJUnitTestCase {
-    private FrameFixture window;
-    private UITestUtils utils;
+    private static final long serialVersionUID = 1L;
+    /**
+     * Az ablakon megjelenő gombok
+     */
+    public static JButton buttons[]= new JButton[2];
+    /**
+     * Az ablakon megjelenő gombok feliratai
+     */
+    public static String titles[] = {"New Game", "Load Game"};
+    /**
+     * A játékosok száma
+     */
+    private int playerCount = 2;
 
-    @Before
-    @Override
-    public void onSetUp() {
-        var frame = GuiActionRunner.execute(MenuFrame::new);
-        window = new FrameFixture(robot(), frame);
-        utils = new UITestUtils(window);
-        window.show();
-    }
+    /**
+     * Konstruktor
+     * */
+    public MenuFrame() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Blind Virologists");
+        setSize(500, 255);
+        setResizable(false);
 
-    @Test
-    public void testWindowTitle(){
-        window.requireTitle("Blind Virologists");
-    }
+        JPanel panel = new JPanel();
+        panel.setAlignmentX(0);
 
-    @Test
-    public void testButtonNewGame() {
-        var newGameButton = utils.getButtonFixtureByText("New Game");
-        newGameButton.requireEnabled();
-        newGameButton.requireVisible();
-        newGameButton.requireText("New Game");
-    }
+        for (int i = 0; i < 2; i++) {
+            buttons[i] = new JButton();
+            buttons[i].setPreferredSize(new Dimension(150, 50));
+            buttons[i].setText(titles[i]);
+            buttons[i].setName(titles[i]);
+            buttons[i].setActionCommand(titles[i]);
+            buttons[i].addActionListener(new StartButtonActionListener());
+            panel.add(buttons[i]);
+        }
 
-    @Test
-    public void testButtonLoadGame() {
-        var loadGameButton = utils.getButtonFixtureByText("Load Game");
-        loadGameButton.requireEnabled();
-        loadGameButton.requireVisible();
-        loadGameButton.requireText("Load Game");
-    }
+        JLabel text = new JLabel("Blind Virologists", SwingConstants.CENTER);
+        text.setBackground(null);
+        text.setPreferredSize(new Dimension(150, 75));
+        text.setFont(new Font("Dialog", Font.BOLD, 50));
 
-    @Test
-    public void testLabelTitle(){
-        var titleLabel = utils.getLabelByText("Blind Virologists");
-        titleLabel.requireVisible();
-        titleLabel.requireEnabled();
-    }
+        JLabel textplayer = new JLabel("Number of players: ", SwingConstants.CENTER);
+        textplayer.setBackground(null);
+        textplayer.setPreferredSize(new Dimension(240, 75));
+        textplayer.setFont(new Font("Dialog", Font.BOLD, 25));
 
-    @Test
-    public void testLabelNumberOfPlayers(){
-        var numberOfPlayersLabel = utils.getLabelByText("Number of players: ");
-        numberOfPlayersLabel.requireVisible();
-        numberOfPlayersLabel.requireEnabled();
-    }
-
-    @Test
-    public void testTextFieldPlayerCount(){
-        var playerCountTextField = utils.getTextComponentByText("2");
-        playerCountTextField.requireVisible();
-        playerCountTextField.requireEnabled();
-        playerCountTextField.requireEditable();
-        playerCountTextField.requireText("2");
-        playerCountTextField.click();
-        playerCountTextField.requireFocused();
-        playerCountTextField.robot().pressKey(KeyEvent.VK_BACK_SPACE);
-        playerCountTextField.requireText("");
-        playerCountTextField.robot().enterText("123");
-        playerCountTextField.requireText("123");
-    }
-
-    @Test
-    public void testButtonClickNewGame() {
-        utils.getButtonFixtureByText("New Game").click();
-        findFrame(new GenericTypeMatcher<>(Frame.class) {
-            protected boolean isMatching(Frame frame) {
-                return "Blind Virologists".equals(frame.getTitle()) && frame.isShowing() && frame instanceof GameFrame;
+        JTextField player = new JTextField("2",5);
+        player.addKeyListener(new KeyAdapter() {
+            public void keyTyped(KeyEvent e){
+                if((e.getKeyChar() < '0' || e.getKeyChar() > '9') && e.getKeyChar() != '\b' || player.getText().length() > 9){
+                    e.consume();
+                }
             }
-        }).using(robot());
+        });
+
+        player.getDocument().addDocumentListener(new DocumentListener()
+        {
+            public void changedUpdate(DocumentEvent arg0)
+            {
+                playerCount = Integer.parseInt(player.getText());
+            }
+            public void insertUpdate(DocumentEvent arg0)
+            {
+                playerCount = Integer.parseInt(player.getText());
+            }
+            public void removeUpdate(DocumentEvent arg0)
+            {
+                if (player.getText().length() > 0)
+                    playerCount = Integer.parseInt(player.getText());
+                else
+                    playerCount = 2;
+            }
+        });
+        player.setPreferredSize(new Dimension(35, 25));
+
+        JPanel south = new JPanel();
+        south.setBounds(40,80,400,100);
+        south.add(textplayer);
+        south.add(player);
+
+        add(text, BorderLayout.NORTH);
+        add(panel, BorderLayout.CENTER);
+        add(south, BorderLayout.SOUTH);
+        setLocationRelativeTo(null);
+        setVisible(true);
     }
 
-    @Test
-    public void testTextFieldPlayerCountOnlyNumericalValueAllowed() {
-        var playerCountTextField = utils.getTextComponentByText("2");
-        playerCountTextField.click();
-        playerCountTextField.robot().enterText("The");
-        playerCountTextField.requireText("2");
-    }
+    /**
+     * GETTERS SETTERS
+     */
 
-    @Test
-    public void testTextFieldPlayerCountMaxLengthLimited() {
-        var playerCountTextField = utils.getTextComponentByText("2");
-        playerCountTextField.click();
-        playerCountTextField.robot().pressKey(KeyEvent.VK_BACK_SPACE);
-        playerCountTextField.robot().enterText("012345678910");
-        playerCountTextField.requireText("0123456789");
+    public int getPlayerCount() {
+        return playerCount;
+    }
+    public void setPlayerCount(int playerCount) {
+        this.playerCount = playerCount;
     }
 }
